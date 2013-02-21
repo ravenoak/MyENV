79db1a82d7caefc91a0f66b513dc58667e275e0d
Caitlyn O'Hanna <ravenoak@virtualxistenz.com>
2013-02-20 17:32:06 -0800

    Starting, but don't have enough time to finish at work.

--- src/lib/myenv.sh
+++ src/lib/myenv.sh
@@ -3,3 +3,7 @@
 # File not intended to be ran standalone #
 ##########################################
 
+##
+# So... (( )) works in bash 4 and 3, so we can use that even if we aren't using
+# new bash in MacOSX
+##
